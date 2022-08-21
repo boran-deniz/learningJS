b3bf476d809b2cf6ffcf25840e5ec5fe7872655b
Boran Ayhan Deniz <boran.deniz@outlook.de>
2022-08-21 17:37:23 +0200

    add camelize task

--- Data Types/Array Methods/camelize.ts
+++ Data Types/Array Methods/camelize.ts
@@ -11,10 +11,20 @@ camelize("list-style-image") == 'listStyleImage';
 camelize("-webkit-transition") == 'WebkitTransition';
 P.S. Hint: use split to split the string into an array, transform it and join back. */
 
-function camelize(str){
-    let arr = str.split("-");
-    let secStr = arr[1];
-    let newSecStr = secStr[0].toUpperCase();
-}
+function stringSplit(string: string){
+    let arr: string[] = string.split("-");
+    let newarr: string[] = [];
+    arr.forEach(function(a, index){
+        index = arr.indexOf(a);
+        if (index !== 0){
+            newarr.push(a[0].toUpperCase());
+            newarr.push(a.slice(1));
+        } else{
+            newarr.push(a)
+        }
+    })
+    let combinedString: string = newarr.join("");
+    return combinedString;
+};
 
-console.log(camelize("background-color"));
+console.log(stringSplit("-webkit-transition"));
